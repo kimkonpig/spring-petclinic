fd1138b9d563e7d2287c8bcde8a0cda1e56707cd
kimkonpig <linggooling@gmail.com>
2020-12-28 14:38:01 +0900

    aop practice

--- src/main/java/org/springframework/samples/petclinic/owner/LogAspect.java
+++ src/main/java/org/springframework/samples/petclinic/owner/LogAspect.java
@@ -15,16 +15,20 @@ https://atoz-develop.tistory.com/entry/Spring-%EC%8A%A4%ED%94%84%EB%A7%81-AOP-%E
 
 @Component
 @Aspect
+// Aspect 역할 클래스 명시
+// xml 설정 파일에 <aop:aspectj-autoproxy/> 입력 필요
+// @Aspect 어노테이션이 있는 클래스를 자동으로 찾아서 설정해주는 태그
 public class LogAspect {
 
 	Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
-	@Around("@annotation(LogExecutionTime)")
+	@Around("@annotation(LogExecutionTime)") // Around가 적용될 PointCut을 명시 :
+												// @LogExecutionTime 어노테이션
 	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 
-		Object proceed = joinPoint.proceed();
+		Object proceed = joinPoint.proceed(); // 핵심 기능 실행
 
 		stopWatch.stop();
 		logger.info(stopWatch.prettyPrint());
